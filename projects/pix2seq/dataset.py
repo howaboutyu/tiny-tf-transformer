ac5713d51f690a7318c5b20861689086bf7125f3
howaboutyu <higgsboost@gmail.com>
2023-11-11 19:45:36 +0800

    Format

--- projects/pix2seq/dataset.py
+++ projects/pix2seq/dataset.py
@@ -3,14 +3,15 @@ import tensorflow_datasets as tfds
 import numpy as np
 from utils import visualize_detections
 
-PAD_VALUE = 0 
+PAD_VALUE = 0
 
-# TODO: DELETE 
-#int2str = dataset_info.features["objects"]["label"].int2str
-#print(int2str)
+# TODO: DELETE
+# int2str = dataset_info.features["objects"]["label"].int2str
+# print(int2str)
 #
-#names_list = tf.constant(dataset_info.features["objects"]["label"].names)
+# names_list = tf.constant(dataset_info.features["objects"]["label"].names)
 # mapping function from tf int to string
+
 
 def resize_image(image, bboxes, max_side=512):
     """
@@ -55,92 +56,89 @@ def resize_image(image, bboxes, max_side=512):
 
     return image, bboxes
 
-    
-def preprocess_fn( data, max_side=512, num_bins=256):
 
+def preprocess_fn(data, max_side=512, num_bins=256):
     label = data["objects"]["label"]
     image = data["image"]
     bboxes = data["objects"]["bbox"]
 
     # [x,y,width,height]
-    bboxes = tf.cast( bboxes, tf.float32 )
-    image = tf.cast( image, tf.float32 )
+    bboxes = tf.cast(bboxes, tf.float32)
+    image = tf.cast(image, tf.float32)
 
     # get shape
-    image_shape = tf.shape( image )  
-    image_width = tf.cast( image_shape[1], tf.float32 )
-    image_height = tf.cast( image_shape[0], tf.float32 )
+    image_shape = tf.shape(image)
+    image_width = tf.cast(image_shape[1], tf.float32)
+    image_height = tf.cast(image_shape[0], tf.float32)
 
-    bboxes = tf.cast( bboxes, tf.float32 )
+    bboxes = tf.cast(bboxes, tf.float32)
 
-    x_min = bboxes[:,1] * image_width
-    y_min = bboxes[:,0] * image_height
-    x_max = bboxes[:,3] * image_width
-    y_max = bboxes[:,2] * image_height
+    x_min = bboxes[:, 1] * image_width
+    y_min = bboxes[:, 0] * image_height
+    x_max = bboxes[:, 3] * image_width
+    y_max = bboxes[:, 2] * image_height
 
-    bboxes = tf.stack( [x_min,y_min,x_max,y_max], axis=1 )
-    bboxes = tf.cast( bboxes, tf.int32 )
+    bboxes = tf.stack([x_min, y_min, x_max, y_max], axis=1)
+    bboxes = tf.cast(bboxes, tf.int32)
 
-    image, bboxes = resize_image(image, bboxes, max_side=max_side) 
+    image, bboxes = resize_image(image, bboxes, max_side=max_side)
 
     # pad image
-    image = tf.image.pad_to_bounding_box( image, 0, 0, max_side, max_side)
+    image = tf.image.pad_to_bounding_box(image, 0, 0, max_side, max_side)
 
-    bboxes = coord_to_bins( bboxes, max_side, num_bins=num_bins )
+    bboxes = coord_to_bins(bboxes, max_side, num_bins=num_bins)
     bboxes = tf.cast(bboxes, tf.int32)
 
     return image, bboxes, label, image_shape
 
+
 def coord_to_bins(coord, image_dim, num_bins):
-    bin = tf.cast(  coord / (image_dim / (num_bins - 1) ), tf.int32 )
+    bin = tf.cast(coord / (image_dim / (num_bins - 1)), tf.int32)
 
     bin = bin + 1
     return bin
+
 
 def bins_to_coord(bin, image_dim, num_bins):
     if isinstance(bin, tf.Tensor):
         bin = bin.numpy()
 
     bin = bin - 1
-    coord = (bin-1) * (image_dim / num_bins)
+    coord = (bin - 1) * (image_dim / num_bins)
     return coord
 
-def format_fn(image, bboxes, label, image_shape, EOS_TOKEN, max_objects=40):
 
-    num_objects = tf.shape( bboxes )[0]
-    sequence_ids = tf.range( num_objects )
+def format_fn(image, bboxes, label, image_shape, EOS_TOKEN, max_objects=40):
+    num_objects = tf.shape(bboxes)[0]
+    sequence_ids = tf.range(num_objects)
 
     # shuffle
-    random_index = tf.random.shuffle( sequence_ids )
+    random_index = tf.random.shuffle(sequence_ids)
 
-    random_bboxes = tf.gather( bboxes, random_index )
-    random_labels = tf.gather( label, random_index ) 
+    random_bboxes = tf.gather(bboxes, random_index)
+    random_labels = tf.gather(label, random_index)
 
-    random_bboxes = tf.reshape( random_bboxes, (-1,4) )
-    random_labels = tf.reshape( random_labels, (-1,1) )
-
+    random_bboxes = tf.reshape(random_bboxes, (-1, 4))
+    random_labels = tf.reshape(random_labels, (-1, 1))
 
     random_labels = tf.cast(random_labels, tf.int32)
-    target = tf.concat( [random_bboxes, random_labels], axis=1 )
+    target = tf.concat([random_bboxes, random_labels], axis=1)
 
     # flatten target
-    target = tf.reshape( target, (-1,) )
-    target = tf.concat( [target, [EOS_TOKEN]], axis=0 )
+    target = tf.reshape(target, (-1,))
+    target = tf.concat([target, [EOS_TOKEN]], axis=0)
 
     decoder_input = target[..., :-1]
     decoder_output = target[..., 1:]
 
-    # limit input and target  
-    if tf.shape(decoder_output)[0] > max_objects*5:
-        decoder_input = decoder_input[..., :max_objects*5]
-        decoder_output = decoder_output[..., :max_objects*5]
+    # limit input and target
+    if tf.shape(decoder_output)[0] > max_objects * 5:
+        decoder_input = decoder_input[..., : max_objects * 5]
+        decoder_output = decoder_output[..., : max_objects * 5]
 
-
-    elif tf.shape(decoder_output)[0] < max_objects*5:
-        pad = max_objects*5 - tf.shape(decoder_output)[0]
-        decoder_input = tf.pad( decoder_input, [[0,pad]], constant_values=PAD_VALUE )
-        decoder_output = tf.pad( decoder_output, [[0,pad]], constant_values=PAD_VALUE )
-
+    elif tf.shape(decoder_output)[0] < max_objects * 5:
+        pad = max_objects * 5 - tf.shape(decoder_output)[0]
+        decoder_input = tf.pad(decoder_input, [[0, pad]], constant_values=PAD_VALUE)
+        decoder_output = tf.pad(decoder_output, [[0, pad]], constant_values=PAD_VALUE)
 
     return (image, decoder_input), decoder_output
-
--- projects/pix2seq/trainer.py
+++ projects/pix2seq/trainer.py
@@ -1,4 +1,4 @@
-from dataset import preprocess_fn, format_fn 
+from dataset import preprocess_fn, format_fn
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import numpy as np
@@ -7,55 +7,52 @@ from tiny_tf_transformer.transformer import Decoder
 from tiny_tf_transformer.losses_and_metrics import (
     masked_sparse_categorical_cross_entropy,
     masked_sparse_categorical_accuracy,
-    )
+)
 
-(train_ds, val_ds), dataset_info = tfds.load( "coco/2017", split=["train", "validation"], with_info=True, data_dir="~/data" )
+(train_ds, val_ds), dataset_info = tfds.load(
+    "coco/2017", split=["train", "validation"], with_info=True, data_dir="~/data"
+)
 
-max_side = 128 
-num_bins = 128 
-batch_size = 2 
-max_objects =10 
+max_side = 128
+num_bins = 128
+batch_size = 2
+max_objects = 10
 
 EOS_TOKEN = num_bins + 1
 
-train_ds = train_ds.map( 
-    lambda x: preprocess_fn(x, max_side=max_side, num_bins=num_bins), 
-    num_parallel_calls=tf.data.experimental.AUTOTUNE )
+train_ds = train_ds.map(
+    lambda x: preprocess_fn(x, max_side=max_side, num_bins=num_bins),
+    num_parallel_calls=tf.data.experimental.AUTOTUNE,
+)
 
 train_ds = train_ds.map(
-    lambda image, bboxes, label, image_shape: format_fn(image, bboxes, label, image_shape, EOS_TOKEN, max_objects=max_objects),
-    )
+    lambda image, bboxes, label, image_shape: format_fn(
+        image, bboxes, label, image_shape, EOS_TOKEN, max_objects=max_objects
+    ),
+)
 
-train_ds = train_ds.batch( batch_size )
-
-
+train_ds = train_ds.batch(batch_size)
 
 
 def get_model(
-        input_shape, 
-        model_name="resnet50v2",
-        num_layers: int = 4,
-        d_model: int = 128,
-        num_heads: int = 8,
-        d_ff: int = 512,
-        target_vocab_size: int = 256,
-        attention_dropout_rate: float = 0.1,
-        ff_dropout_rate: float = 0.1,
-        max_length: int = 2048,
-
-        ):
-
+    input_shape,
+    model_name="resnet50v2",
+    num_layers: int = 4,
+    d_model: int = 128,
+    num_heads: int = 8,
+    d_ff: int = 512,
+    target_vocab_size: int = 256,
+    attention_dropout_rate: float = 0.1,
+    ff_dropout_rate: float = 0.1,
+    max_length: int = 2048,
+):
     if model_name == "resnet50v2":
-        feature_extractor = tf.keras.applications.ResNet50V2( include_top=False, weights="imagenet" )   
+        feature_extractor = tf.keras.applications.ResNet50V2(
+            include_top=False, weights="imagenet"
+        )
         preprocess_fn = tf.keras.applications.resnet_v2.preprocess_input
 
-    input = tf.keras.layers.Input( shape=input_shape )
-    decoder_input = tf.keras.layers.Input( shape=(max_length,) )
-
-    x = preprocess_fn( input )
-
-    x = feature_extractor(x)
-
+    # get decoder model
     decoder = Decoder(
         num_layers=num_layers,
         d_model=d_model,
@@ -65,25 +62,31 @@ def get_model(
         attention_dropout_rate=attention_dropout_rate,
         ff_dropout_rate=ff_dropout_rate,
         max_length=max_length,
-        )
+    )
 
-    final_layer = tf.keras.layers.Dense(target_vocab_size, name="final_layer")
+    input = tf.keras.layers.Input(shape=input_shape)
+    decoder_input = tf.keras.layers.Input(shape=(max_length,))
+
+    x = preprocess_fn(input)
+
+    # extract features
+    x = feature_extractor(x)
 
     # reshape context from [n, n, d] to [n * n, d]
-    x = tf.keras.layers.Reshape( (-1, x.shape[-1]) )(x)
+    x = tf.keras.layers.Reshape((-1, x.shape[-1]))(x)
 
     # reduce dimensionality
-    x = tf.keras.layers.Dense( d_model, activation="relu" )(x) 
+    x = tf.keras.layers.Dense(d_model, activation="relu")(x)
 
-    x = decoder( decoder_input, x ) 
+    # use a causal decoder 
+    x = decoder(decoder_input, x)
 
-    x = final_layer(x)
+    x = tf.keras.layers.Dense(target_vocab_size, name="final_layer")(x)
 
-    model = tf.keras.Model( inputs=[input, decoder_input], outputs=x )
+    model = tf.keras.Model(inputs=[input, decoder_input], outputs=x)
 
     print(model.summary())
     return model
-
 
 
 input_shape = (max_side, max_side, 3)
@@ -95,36 +98,30 @@ model = get_model(
     d_model=128,
     num_heads=8,
     d_ff=512,
-    target_vocab_size=num_bins+2,
+    target_vocab_size=num_bins + 2,
     attention_dropout_rate=0.1,
     ff_dropout_rate=0.1,
-    max_length=max_objects*5,
-    )
+    max_length=max_objects * 5,
+)
 
 
 optimizer = tf.keras.optimizers.Adam(
     learning_rate=1e-4,
-    )
+)
 
 model.compile(
     loss=masked_sparse_categorical_cross_entropy,
     optimizer=optimizer,
     metrics=[masked_sparse_categorical_accuracy],
-    )
+)
 
 model.fit(
     train_ds.take(1000),
     epochs=10,
     validation_data=train_ds.take(1),
-    )
+)
 
 
 for t in train_ds.take(10):
     (image, decoder_input), decoder_output = t
     print(np.min(decoder_output))
-    #print(image.shape)
-    #print(decoder_output.shape)
-    #print(decoder_input.shape)
-
-
-
