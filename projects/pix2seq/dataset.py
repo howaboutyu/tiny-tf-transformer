374c29123e7596a77eb0a03cbdf4408037fde222
howaboutyu <higgsboost@gmail.com>
2023-11-25 18:06:48 +0800

    added omegaconf

--- projects/pix2seq/dataset.py
+++ projects/pix2seq/dataset.py
@@ -3,7 +3,7 @@ import tensorflow_datasets as tfds
 import numpy as np
 from utils import visualize_detections
 
-PAD_VALUE =  0 
+PAD_VALUE = 0
 
 # TODO: DELETE
 # int2str = dataset_info.features["objects"]["label"].int2str
@@ -81,17 +81,18 @@ def preprocess_fn(data, max_side=512, num_bins=256):
 
     bboxes = tf.stack([x_min, y_min, x_max, y_max], axis=1)
 
-    image = tf.image.resize(image, (max_side, max_side)) 
+    image = tf.image.resize(image, (max_side, max_side))
 
-    #image, bboxes = resize_image(image, bboxes, max_side=max_side)
+    # image, bboxes = resize_image(image, bboxes, max_side=max_side)
 
     # pad image
-    #image = tf.image.pad_to_bounding_box(image, 0, 0, max_side, max_side)
+    # image = tf.image.pad_to_bounding_box(image, 0, 0, max_side, max_side)
 
     # quantize bboxes
     bboxes = quantize(bboxes, bins=num_bins)
 
     return image, bboxes, label, image_shape
+
 
 def quantize(x, bins=1000):
     # x is a real number between [0, 1]
@@ -110,7 +111,8 @@ def dequantize(x, bins=1000):
     else:
         return float(x) / (bins - 1)
 
-def format_fn(image, bboxes, label, image_shape, EOS_TOKEN, max_objects=40):
+
+def format_fn(image, bboxes, label, image_shape, sos_token, eos_token, max_objects=40):
     num_objects = tf.shape(bboxes)[0]
     sequence_ids = tf.range(num_objects)
 
@@ -125,11 +127,19 @@ def format_fn(image, bboxes, label, image_shape, EOS_TOKEN, max_objects=40):
 
     random_labels = tf.cast(random_labels, tf.int32)
     target = tf.concat([random_bboxes, random_labels], axis=1)
+
+    """
+    bboxes = tf.reshape(bboxes, (-1, 4))
+    label = tf.reshape(label, (-1, 1))
+    label = tf.cast(label, tf.int32)
+    target = tf.concat([bboxes, label], axis=1)
+    """
+    # add one to labels as 0 is reserved for padding
     target += 1
 
     # flatten target
     target = tf.reshape(target, (-1,))
-    target = tf.concat([target, [EOS_TOKEN]], axis=0)
+    target = tf.concat([[sos_token], target, [eos_token] * 5], axis=0)
 
     decoder_input = target[..., :-1]
     decoder_output = target[..., 1:]
@@ -143,5 +153,59 @@ def format_fn(image, bboxes, label, image_shape, EOS_TOKEN, max_objects=40):
         pad = max_objects * 5 - tf.shape(decoder_output)[0]
         decoder_input = tf.pad(decoder_input, [[0, pad]], constant_values=PAD_VALUE)
         decoder_output = tf.pad(decoder_output, [[0, pad]], constant_values=PAD_VALUE)
-    
+
     return (image, decoder_input), decoder_output
+
+
+def sequence_decoder(decoder_output, eos_token, num_bins):
+    """
+    Decodes the output of a sequence decoder into bounding boxes and labels.
+
+    Args:
+    decoder_output (Tensor): The output from the decoder model.
+    eos_token (int): The token representing the end of a sequence.
+    num_bins (int): Number of bins used for dequantization.
+
+    Returns:
+    tuple: A tuple containing lists of filtered bounding boxes and labels.
+    """
+
+    # Extract the batch size from the decoder's output shape
+    batch_size = tf.shape(decoder_output)[0]
+
+    # Reshape the decoder output to separate the bounding box coordinates and labels
+    decoder_output = tf.reshape(decoder_output, (batch_size, -1, 5))
+
+    # Extract bounding boxes and labels from the decoder output
+    bboxes = decoder_output[..., :4]  # shape: (batch_size, num_objects, 4)
+    labels = decoder_output[..., 4:]  # shape: (batch_size, num_objects, 1)
+
+    # Initialize lists to store the filtered bounding boxes and labels
+    bboxes_filtered = []
+    labels_filtered = []
+
+    # Process each item in the batch
+    for i in range(batch_size):
+        # Identify the first occurrence of the end-of-sequence token in labels
+        index = tf.where(tf.equal(labels[i], eos_token))
+
+        # Check if the end-of-sequence token is found
+        if tf.shape(index)[0] != 0:
+            # Use the index of the EOS token to filter the bounding boxes and labels
+            index = index[0, 0]
+            bboxes_tmp = bboxes[i, :index] - 1
+            labels_tmp = labels[i, :index] - 1
+        else:
+            # If EOS token is not found, use all boxes and labels
+            bboxes_tmp = bboxes[i] - 1
+            labels_tmp = labels[i] - 1
+
+        # Dequantize the bounding boxes
+        bboxes_tmp = dequantize(bboxes_tmp, bins=num_bins)
+
+        # Append the processed boxes and labels to the respective lists
+        bboxes_filtered.append(bboxes_tmp)
+        labels_filtered.append(labels_tmp)
+
+    # Return the filtered bounding boxes and labels
+    return bboxes_filtered, labels_filtered
--- projects/pix2seq/main.py
+++ projects/pix2seq/main.py
@@ -1,7 +1,7 @@
-from dataset import preprocess_fn, format_fn, PAD_VALUE
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import numpy as np
+from omegaconf import OmegaConf
 
 from tiny_tf_transformer.transformer import Decoder
 from tiny_tf_transformer.losses_and_metrics import (
@@ -10,49 +10,61 @@ from tiny_tf_transformer.losses_and_metrics import (
 )
 
 from model import get_pix2seq_model
+from dataset import preprocess_fn, format_fn, PAD_VALUE, sequence_decoder
+from utils import visualize_detections
 
 
-
-
-
-def get_dataset(batch_size=32, max_side=512, num_bins=256, max_objects=20):
+def get_dataset(data_config):
+    # Load the dataset using TensorFlow Datasets
     (train_ds, val_ds), dataset_info = tfds.load(
-        "coco/2017", split=["train", "validation"], with_info=True, data_dir="~/data"
+        "coco/2017", 
+        split=["train", "validation"], 
+        with_info=True, 
+        data_dir=data_config.data_dir  # Use data directory from the config
     )
 
-    EOS_TOKEN = num_bins + 1
+    # Define special tokens based on num_bins from the config
+    sos_token = data_config.num_bins + 1
+    eos_token = data_config.num_bins + 2
 
+    max_side = data_config.max_side
+    num_bins = data_config.num_bins
+    max_objects = data_config.max_objects
+
+    # Function to apply preprocessing and formatting
+    def preprocess_and_format(x):
+        # Preprocess the data
+        preprocessed = preprocess_fn(
+            x, 
+            max_side=max_side,
+            num_bins=num_bins
+        )
+        # Format the data
+        return format_fn(
+            *preprocessed, 
+            sos_token=sos_token, 
+            eos_token=eos_token, 
+            max_objects=max_objects
+        )
+
+    # Apply the preprocessing and formatting functions to the datasets
     train_ds = train_ds.map(
-        lambda x: preprocess_fn(x, max_side=max_side, num_bins=num_bins),
-        num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        preprocess_and_format,
+        num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
 
     val_ds = val_ds.map(
-        lambda x: preprocess_fn(x, max_side=max_side, num_bins=num_bins),
-        num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        preprocess_and_format,
+        num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
 
-    train_ds =train_ds.map(
-        lambda image, bboxes, label, image_shape: format_fn(
-            image, bboxes, label, image_shape, EOS_TOKEN, max_objects=max_objects
-        ),
-    )
+    # Batch the datasets
+    train_ds = train_ds.batch(data_config.batch_size)
+    val_ds = val_ds.batch(data_config.batch_size)
 
-    val_ds =val_ds.map(
-        lambda image, bboxes, label, image_shape: format_fn(
-            image, bboxes, label, image_shape, EOS_TOKEN, max_objects=max_objects
-        ),
-    )
-
-    train_ds = train_ds.batch(batch_size)
-    val_ds = val_ds.batch(batch_size)
-
-
-
-    return train_ds, val_ds
+    return train_ds, val_ds, sos_token, eos_token
 
 def train(train_ds, val_ds, max_side=512, num_bins=256, max_objects=20):
-    
     input_shape = (max_side, max_side, 3)
 
     model = get_pix2seq_model(
@@ -85,13 +97,25 @@ def train(train_ds, val_ds, max_side=512, num_bins=256, max_objects=20):
     )
 
 
+if __name__ == "__main__":
+    config = OmegaConf.load("config.yaml")
 
-if __name__ == '__main__':
-    train_ds, val_ds = get_dataset()
+    train_ds, val_ds, sos_token, eos_token = get_dataset(
+        config.data_config
+        
+    )
 
     for (x, y_in), y_out in train_ds.take(1):
-        print(y_out)
-        print(y_out)
-        import pdb; pdb.set_trace()
+        bboxes, labels = sequence_decoder(
+            y_out, eos_token=eos_token, num_bins=config.data_config.num_bins
+        )
+        images = x
+
+        for i in range(config.data_config.batch_size):
+            bboxes_np = np.asarray(bboxes[i]) * config.data_config.max_side
+            labels_np = np.asarray(labels[i])
+            scores = np.ones_like(labels_np).astype(np.float32)
+
+            visualize_detections(images[i], bboxes_np, labels_np, scores)
 
     train(train_ds, val_ds)
